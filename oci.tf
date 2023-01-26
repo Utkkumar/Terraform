0a9d8007e32c2b484b56ed9c091b2b2df69cea44
Utkkumar <117896821+Utkkumar@users.noreply.github.com>
2023-01-26 20:58:26 +0530

    Update oci.tf

--- oci.tf
+++ oci.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-provider "oracle/oci"  {
+provider "oci"  {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
