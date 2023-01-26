0231aff92e229fc724d19aea7b057b5d1a9ce332
Utkkumar <117896821+Utkkumar@users.noreply.github.com>
2023-01-26 20:42:58 +0530

    Update oci.tf

--- oci.tf
+++ oci.tf
@@ -6,11 +6,12 @@ terraform {
     }
   }
 }
-provider "oci"  {
+provider "oracle/oci"  {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   region           = var.region
+  private_key_path = var.private_key_path
 }
 
 #Step-1 Create Root Compartment
