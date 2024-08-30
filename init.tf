f5282e988ccaff9483867efc3b451aa282a5e598
Utkkumar <117896821+Utkkumar@users.noreply.github.com>
2024-08-30 16:31:46 +0530

    Update init.tf

--- init.tf
+++ init.tf
@@ -1,12 +1,20 @@
-resource "null_resource" "init_trigger" {
-  triggers = {
-    reconfigure = "${timestamp()}" 
-  }
-  provisioner "local-exec" {
-    command = "echo 'Initialization complete'"
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
+variable "force_recreate" {
+ description = "Set this to true to force recreation of the null resource"
+ type        = bool
+ default     = false
 }
-
+resource "null_resource" "init_trigger" {
+ # The triggers block ensures that the resource is recreated when force_recreate is true
+ triggers = {
+   reconfigure = "${tostring(var.force_recreate)}"
+ }
+ provisioner "local-exec" {
+   command = "echo 'Initialization complete'"
+ }
+ lifecycle {
+   create_before_destroy = true
+ }
+}
+output "recreate_status" {
+ value = "Resource was ${var.force_recreate ? "recreated" : "not recreated"}"
+}
