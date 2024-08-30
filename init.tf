4f2ef1a9ee33a13660a35eec7d97d01e6ccb0561
Utkkumar <117896821+Utkkumar@users.noreply.github.com>
2024-08-30 16:23:36 +0530

    Update init.tf

--- init.tf
+++ init.tf
@@ -1,5 +1,12 @@
 resource "null_resource" "init_trigger" {
+  triggers = {
+    reconfigure = "${timestamp()}" 
+  }
   provisioner "local-exec" {
     command = "echo 'Initialization complete'"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
